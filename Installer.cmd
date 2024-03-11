81e531d48a6ef7054865601420baa8c7fc62cc45
floofydude68 <116403430+floofydude68@users.noreply.github.com>
2024-03-11 08:21:35 -0400

    Add files via upload

---
+++ Installer.cmd
@@ -0,0 +1,57 @@
+@echo off
+
+echo.
+
+echo.
+echo Starting optimizer installation...
+echo.
+
+for /d %%i in ("%localappdata%\Roblox\Versions\*") do (
+    if exist "%%i\RobloxPlayerBeta.exe" (
+        set folder=%%i
+        goto :NextStep
+    )
+)
+
+for /d %%i in ("C:\Program Files (x86)\Roblox\Versions\*") do (
+    if exist "%%i\RobloxPlayerBeta.exe" (
+        set folder=%%i
+        goto :NextStep
+    )
+)
+
+for /d %%i in ("C:\Program Files\Roblox\Versions\*") do (
+    if exist "%%i\RobloxPlayerBeta.exe" (
+        set folder=%%i
+        goto :NextStep
+    )
+)
+
+:NextStep
+if not exist "%folder%\ClientSettings" (
+    mkdir "%folder%\ClientSettings"
+)
+
+echo Downloading cool files...
+powershell.exe -Command "& {(New-Object System.Net.WebClient).DownloadFile('https://github.com/floofydude68/R_G-s-roblox-optimizer/blob/df0d069f6ea229217ae468b648797a446ebc196d/ClientAppSettings.json', '%folder%\ClientSettings\ClientAppSettings.json')}"
+if %errorlevel% EQU 0 (
+    echo Cool files downloaded successfully
+    echo.
+    echo SUCCESS: installation completed good job
+) else (
+    echo Failed to download ClientAppSettings.json.
+    echo.
+    echo ERROR: installation failed wtf did you do???
+)
+
+echo.
+echo NOTE:
+echo.
+echo 1. I would be surprised if this worked the 1st time
+echo.
+echo 2. If you need help, DM r_g on discord
+echo.
+echo 3. To uninstall delete roblox and install it again
+echo.
+echo.
+echo Press any key to continue... & pause >nul
